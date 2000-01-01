52d4a6d998c47738e520be436d82035c0c67b848
Jason Kridner <jdk@ti.com>
2000-01-01 00:05:16 +0000

    config.js: Set LED defaults to Black

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.twitterKey = "Consumer key";
 exports.twitterSecret = "Consumer secret";
 exports.accessToken = "Access token";
 exports.tokenSecret = "Access token secret";
-exports.brightness = "Indicator LED brightness file";
-exports.trigger = "Indicator LED trigger file";
+exports.brightness = "/sys/class/leds/beaglebone\:green\:usr3/brightness";
+exports.trigger = "/sys/class/leds/beaglebone\:green\:usr3/trigger";
 exports.message="New #BeagleBoneBlack @BeagleStache image captured!"
 
