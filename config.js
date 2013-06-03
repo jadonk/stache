1c4a540ce5470ff1103207829d325353924df197
Jason Kridner <jdk@ti.com>
2013-06-03 12:35:52 -0400

    config.js: added example

---
+++ config.js
@@ -0,0 +1,8 @@
+exports.twitterKey = "Consumer key";
+exports.twitterSecret = "Consumer secret";
+exports.accessToken = "Access token";
+exports.tokenSecret = "Access token secret";
+exports.brightness = "Indicator LED brightness file";
+exports.trigger = "Indicator LED trigger file";
+exports.message="New #BeagleBoneBlack @BeagleStache image captured!"
+
